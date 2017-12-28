a6e43c2c9ed11052793320efcef7d42a959e1d8b
Morioka Shuhei <selmertsx@gmail.com>
2017-12-28 18:32:16 +0900

    set direnv settings

---
+++ bin/set_env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm .envrc.local
+SlackToken=(`aws ssm get-parameters --names SlackToken --query "Parameters[*].{Value: Value}"  --output text`)
+IAMRole=(`aws ssm get-parameters --names IAMRole --query "Parameters[*].{Value: Value}"  --output text`)
+
+echo "export SlackToken=${SlackToken}" >> .envrc.local
+echo "export IAMRole=${IAMRole}" >> .envrc.local
+
+direnv allow
