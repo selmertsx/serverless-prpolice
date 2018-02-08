53d79f15948c92b405c29c60064d225394b53cc3
Morioka Shuhei <selmertsx@gmail.com>
2018-02-08 17:58:55 +0900

    create cloudformation base

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Reporter } from "./reporter";
 
 const owner = process.env.Owener;
 
-export async function getReviewers(event, context, callback) {
+export async function get(event, context, callback) {
   const params = qs.parse(event.body);
   const repository = params.text.toString();
   const github = new GitHub(owner, repository);
