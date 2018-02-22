c22b0fcabee0c5400db22def4313194cac23006e
Morioka Shuhei <selmertsx@gmail.com>
2018-02-22 09:10:21 +0900

    create register method in user class

--- src/user.ts
+++ src/user.ts
@@ -1,3 +1,7 @@
+import AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.TableName;
+
 export class User {
   public slackId: string;
   public githubAccount: string;
@@ -5,5 +9,26 @@ export class User {
   constructor(slackId: string, githubAccount: string) {
     this.slackId = slackId;
     this.githubAccount = githubAccount;
+  }
+
+  public register() {
+    const record = {
+      TableName: tableName,
+      Item: {
+        id: this.slackId,
+        thing: this.githubAccount
+      }
+    };
+
+    dynamo.put(record, (err, data) => {
+      if (err) {
+        console.error(
+          "Unable to add device. Error JSON:",
+          JSON.stringify(err, null, 2)
+        );
+      } else {
+        console.log("Added device:", JSON.stringify(data, null, 2));
+      }
+    });
   }
 }
